69de2e9cf91d8c53af154a4f4509cda7bc7fc9a4
Alberto Morales <amorales@amorales-pb-cap.home>
2016-02-15 12:13:37 -0500

    removing files not used

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,16 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('the app', function() {
 
   beforeEach(function() {
     browser.get('app/index.html');
+  });
+
+  it('should redirect to /home', function() {
+    browser.getLocationAbsUrl().then(function(url){
+      expect(url).toEqual('/home')
+    });
   });
 
 });
